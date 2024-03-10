3a8fb751694411d89e61ae0dab5316d8853ed639
Aaditya Raj <aadityaraj085@gmail.com>
2024-03-10 21:52:07 +0530

    problem_1

--- HomeWork/Searching_3/subarray_sum_less_than_s.java
+++ HomeWork/Searching_3/subarray_sum_less_than_s.java
@@ -2,6 +2,7 @@ package HomeWork.Searching_3;
 import java.util.*;
 
 public class subarray_sum_less_than_s {
+    // Function to check if it is possible to make subarrays of size mid such that sum of these subarrays are less than or equal to s.
     public static boolean isValid(int[] arr, int n, int s, int mid){
         int size = 0;
         int sum = 0;
@@ -34,10 +35,10 @@ public class subarray_sum_less_than_s {
         while(low <= high){
             int mid = low + (high - low)/2;
 
-            if(isValid(arr, n, s, mid)){
+            if(isValid(arr, n, s, mid)){ // If mid is a valid answer then store it and move to right part as overall goal is to maximize value of X.
                 low = mid + 1;
                 ans = mid;
-            } else{
+            } else{ // else move to left part
                 high = mid - 1;
             }
         }
