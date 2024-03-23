e8162999edf3d17abc2ab287646606e995bdb9ed
Aaditya Raj <aadityaraj085@gmail.com>
2024-03-23 20:27:24 +0530

    problem_8

---
+++ HomeWork/Tree_1_and_2/top_view_bt.java
@@ -0,0 +1,77 @@
+package HomeWork.Tree_1_and_2;
+import java.util.*;
+
+
+// Iterative solution, T.C: O(N), S.C: O(N)
+class Pair{
+    Node node;
+    int y;
+    public Pair(Node node, int y){
+        this.node = node;
+        this.y = y;
+    }
+}
+class Solution
+{
+    //Function to return a list of nodes visible from the top view 
+    //from left to right in Binary Tree.
+    static ArrayList<Integer> topView(Node root)
+    {
+        TreeMap<Integer, Integer> map = new TreeMap<>();
+        Queue<Pair> q = new LinkedList<>();
+        ArrayList<Integer> ans = new ArrayList<>();
+        
+        q.add(new Pair(root, 0));
+        
+        while(!q.isEmpty()){
+            Pair curr =q.poll();
+            map.putIfAbsent(curr.y, curr.node.data);
+            
+            if(curr.node.left!=null) q.add(new Pair(curr.node.left, curr.y-1));
+            if(curr.node.right!=null) q.add(new Pair(curr.node.right, curr.y+1));
+        }
+        
+        for(Map.Entry<Integer, Integer> entry: map.entrySet()){
+            ans.add(entry.getValue());
+        }
+        
+        return ans;
+        
+        
+    }
+}
+
+
+// Recursive solution, S.C: O(N) + O(N)
+class Solution2
+{
+    static void traverse(Node root, TreeMap<Integer, Integer> map, int x){
+        if(root == null){
+            return;
+        }
+        
+        map.putIfAbsent(x, root.data);
+        traverse(root.left, map, x-1);
+        traverse(root.right, map, x+1);
+    }
+    
+    static ArrayList<Integer> topView(Node root)
+    {
+        TreeMap<Integer, Integer> map = new TreeMap<>();
+        ArrayList<Integer> ans = new ArrayList<>();
+        
+        traverse(root, map, 0);
+        
+        for(Map.Entry<Integer, Integer> entry: map.entrySet()){
+            ans.add(entry.getValue());
+        }
+        
+        return ans;
+        
+        
+    }
+}
+
+public class top_view_bt {
+    
+}
