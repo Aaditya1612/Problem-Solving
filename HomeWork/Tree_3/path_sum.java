dd9bbd680f19e7c5263778d8258a897b4de5adbc
Aaditya Raj <aadityaraj085@gmail.com>
2024-03-25 09:55:22 +0530

    problem_10

---
+++ HomeWork/Tree_3/path_sum.java
@@ -0,0 +1,17 @@
+package HomeWork.Tree_3;
+// T.C: O(N), S.C: O(H)
+class Solution {
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        if(root == null){
+            return false;
+        }
+        if(root.val == targetSum && root.left == null && root.right == null){
+            return true;
+        }
+
+        return hasPathSum(root.left, targetSum-root.val) || hasPathSum(root.right, targetSum-root.val);
+    }
+}
+public class path_sum {
+    
+}
