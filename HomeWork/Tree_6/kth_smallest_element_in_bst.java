3656fb0a5d0880af1f7a935619c21e66bdc13677
Aaditya Raj <aadityaraj085@gmail.com>
2024-04-02 22:33:16 +0530

    problem_3

---
+++ HomeWork/Tree_6/kth_smallest_element_in_bst.java
@@ -0,0 +1,29 @@
+package HomeWork.Tree_6;
+
+// T.C: O(N), S.C: O(H)
+class Solution {
+    int ans = 0;
+    int cnt = 0;
+    public void traverse(TreeNode root, int k){
+        if(root == null){
+            return;
+        }
+
+        traverse(root.left, k);
+        cnt++;
+        if(k == cnt){
+            ans = root.val;
+            return;
+        }
+
+        traverse(root.right, k);
+    }
+    public int kthSmallest(TreeNode root, int k) {
+        traverse(root, k);
+        return ans;
+    }
+}
+
+public class kth_smallest_element_in_bst {
+    
+}
