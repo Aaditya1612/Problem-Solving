ddb70fea1202355be2ea317fbce18210fc51b63c
Aaditya Raj <aadityaraj085@gmail.com>
2024-04-28 17:13:17 +0530

    problem_1

---
+++ HomeWork/DP1/climbing_stairs.java
@@ -0,0 +1,26 @@
+package HomeWork.DP1;
+
+class Solution {
+    public int climbStairs(int n) {
+        // 3 -> 2 1
+        // 2 -> 1 0
+
+        if(n == 1){ return 1;}
+        if(n == 2){ return 2;}
+
+        int waysToPreviousStep = 2;
+        int waysToBeforePreviousStep = 1;
+
+        for(int i=3; i<=n; i++){
+            int currSteps = waysToPreviousStep + waysToBeforePreviousStep;
+            waysToBeforePreviousStep = waysToPreviousStep;
+            waysToPreviousStep = currSteps;
+        }
+
+        return waysToPreviousStep; // in the end of the loop this will store the value of nth step
+    }
+}
+
+public class climbing_stairs {
+    
+}
